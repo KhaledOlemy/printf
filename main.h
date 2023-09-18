473d7249e32663121149c610bfc7a29d8e09e44f
khaledolemy <khaledolemy@gmail.com>
2023-09-18 10:13:37 +0300

    main.h added

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef _PRINTF_MAIN_H_
+#define _PRINTF_MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_hex_item(int num, char casing);
+int _print_hexa(unsigned int num, unsigned int iter, int flag, char casing);
+int _print_int(unsigned int num, unsigned int iter, int flag);
+int _print_signed_int(int n);
+int _print_string(char *s);
+int _switcher(const char *s, int i, va_list item, int flag);
+int _print_binary(int n);
+#endif
+#undef _PRINTF_MAIN_H_
